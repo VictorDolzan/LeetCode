da05511cf290e3a9322cea017d47a3a35c960983
VictorDolsan <victorddf5@gmail.com>
2023-08-05 15:55:51 -0300

    Subindo os primeiros Exercicios.cs

--- LeetCode/Exercicios.cs
+++ LeetCode/Exercicios.cs
@@ -2,5 +2,53 @@
 
 public class Exercicios
 {
+    //1ºSt challenge Two Sum
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> dictionary = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if (dictionary.ContainsKey(complement))
+            {
+                var retorno = new int[] {dictionary[complement], i}; 
+                return retorno;
+            }
+
+            dictionary[nums[i]] = i;
+        }
+
+        return Array.Empty<int>();
+    }
+    
+    //2º challenge Richest Customer Wealth
+    public int MaximumWealth(int[][] accounts)
+    {
+        //CÓDIGO 1
+        var somaTotal = new List<int>();
+        foreach (var cliente in accounts)
+        {
+            var soma = 0;
+            foreach (var account in cliente)
+            {
+                soma += account;
+            }
+            somaTotal.Add(soma);
+        }
+        //CÓDIGO 2 
+        for (int i = 0; i < accounts.Length; i++)
+        {
+            var objeto = accounts[i];
+            int soma = 0;
+            for (int j = 0; j < objeto.Length; j++)
+            {
+                soma += objeto[j];
+            }
+            somaTotal.Add(soma);
+        }
+
+        return somaTotal.Max();
+    }
     
 }
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using LeetCode;
+
+var exercicios = new Exercicios();
+
+#region First Challenge Two Sum
+
+/*1º challenge
+var nums = new []{2,7,11,15};
+var target = 9;
+
+var nums = new[] {3, 2, 4};
+var target = 6;
+
+var nums = new[] {3, 2, 7, 5};
+var target = 9;
+
+exercicios.TwoSum(nums, target);*/
+
+#endregion
+
+#region Second Challenge Richest Customer Wealth
+
+int[][] accounts = new int[][]
+{
+    new[] {1, 5},
+    new[] {7, 3},
+    new[] {3, 5}
+};
+
+exercicios.MaximumWealth(accounts);
+#endregion
