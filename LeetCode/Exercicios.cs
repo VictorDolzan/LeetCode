f71c10f5913dad17860b9e1ad3ee996b07610c71
Victor Dolzan <victorddf5@gmail.com>
2024-01-21 15:00:19 -0300

    Task Valid Mountain Array finished

--- LeetCode/Exercicios.cs
+++ LeetCode/Exercicios.cs
@@ -365,4 +365,28 @@ public class Exercicios
         return false;
     }
     #endregion
+    
+    #region 15º ValidMountainArray
+
+    public bool ValidMountainArray(int[] arr)
+    {
+        // new[] { 0,3,2,1};
+        if (arr.Length < 3 || arr[0] > arr[1] || arr[^1] > arr[^2]) return false;
+
+        bool incresing = true;
+        int previous = -1;
+
+        foreach (var item in arr)
+        {
+            if (previous == item) return false;
+            else if (incresing && previous > item) incresing = false;
+            else if (!incresing && previous < item) return false;
+
+            previous = item;
+        }
+
+        return true;
+    }
+
+    #endregion
 }
