29fa6470acf432d71602c41683a7a77e26eee75a
Victor Dolzan <victorddf5@gmail.com>
2024-01-21 17:43:30 -0300

    Exercise Replace Elements with Greatest Element on Right Side finished

--- LeetCode/Exercicios.cs
+++ LeetCode/Exercicios.cs
@@ -370,7 +370,7 @@ public class Exercicios
 
     public bool ValidMountainArray(int[] arr)
     {
-        // new[] { 0,3,2,1};
+        // new[] { 0,2,3,2,1};
         if (arr.Length < 3 || arr[0] > arr[1] || arr[^1] > arr[^2]) return false;
 
         bool incresing = true;
@@ -388,5 +388,37 @@ public class Exercicios
         return true;
     }
 
+    #endregion
+
+    #region 16º Replace Elements with Greatest Element on Right Side
+
+    public int[] ReplaceElements(int[] arr)
+    {
+        // { 17, 18, 5, 4, 6, 1 };
+        var newArray = new int[arr.Length];
+        
+        if (arr.Length == 1) newArray[0] = -1;
+        else
+        {
+            for (int cont = 0; cont < arr.Length; cont++)
+            {
+                var result = -1;
+                for (int contX = cont; contX < arr.Length; contX++)
+                {
+                    if (contX + 1 < arr.Length)
+                    {
+                        if (arr[contX + 1] > result)
+                        {
+                            result = arr[contX + 1];
+                        }   
+                    }
+                }
+                newArray[cont] = result;
+            }
+        }
+        
+        return newArray;
+    }
+    
     #endregion
 }
